55272c2c1e5e1ffea14fb092560bd0d1b5eeaf2a
DoerteWa <doertew99@gmail.com>
2023-11-12 15:32:20 +0100

    edit setup.py and requirements.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ setup(
     version = "1.0",
     packages = find_packages(),
     install_requires = [
-        "numpy",
-        "unittests",
-        "random"
-    ],
+        "numpy"
+                ],
     author = "DoerteWa",
     url = "https://github.com/DoerteWa/dsss_homework_2.git"
 )
