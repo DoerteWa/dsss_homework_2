b42dc77b0950fba68961fa2f9874c0732ec7cd37
DoerteWa <doertew99@gmail.com>
2023-11-12 14:33:59 +0100

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "DSSSHomework2",
+    version = "1.0",
+    packages = find_packages(),
+    author = "Dörte Wartke",
+    url = "https://github.com/DoerteWa/dsss_homework_2.git",
+)
